cfaedea3204599be6616bea2744a34cd789521c9
yl0937 <yl1033371@gmail.com>
2023-03-19 17:25:50 +0900

    feat: 클럽 상세 검색 및 조회수 기능 작성
    * feat: 클럽 상세 검색 Club Controller, Service 작성
    * feat: 클럽 조회수 증가 및 중복 방지 updateCookie 작성

--- src/main/java/com/oinzo/somoim/domain/club/ClubController.java
+++ src/main/java/com/oinzo/somoim/domain/club/ClubController.java
@@ -1,16 +1,18 @@
 package com.oinzo.somoim.domain.club;
 
-import com.oinzo.somoim.common.exception.ErrorResponse;
 import com.oinzo.somoim.domain.club.entity.Club;
 import com.oinzo.somoim.domain.club.service.ClubService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/club")
+@RequestMapping("/clubs")
 public class ClubController {
     private final ClubService clubService;
 
@@ -28,5 +30,14 @@ public class ClubController {
     @GetMapping("/favorite")
     public List<Club> readClubByFavorite(@RequestBody Club request){
         return clubService.readClubByFavorite(request.getFavorite(),request.getArea());
+    }
+
+    @GetMapping("/{clubId}")
+    public Club readClubById(@PathVariable("clubId") Long clubId, HttpServletResponse response,
+                                        @CookieValue(value="count", required=false) Cookie countCookie){
+        Optional<Club> club = clubService.readClubById(clubId);
+        Integer newCnt = clubService.updateCookie(response,countCookie,clubId,club.get().getCnt());
+        clubService.updateCnt(clubId,newCnt);
+        return club.get();
     }
 }
--- src/main/java/com/oinzo/somoim/domain/club/service/ClubService.java
+++ src/main/java/com/oinzo/somoim/domain/club/service/ClubService.java
@@ -6,8 +6,11 @@ import com.oinzo.somoim.domain.club.repository.ClubRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Optional;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -25,6 +28,56 @@ public class ClubService {
 
     public List<Club> readClubByFavorite(String favorite, String area){
         return clubRepository.findAllByFavoriteContainingAndAreaContaining(favorite,area);
+    }
+
+    public Optional<Club> readClubById(Long clubId){
+        return clubRepository.findById(clubId);
+    }
+
+    public List<Club> readClubByArea(String area){
+        return clubRepository.findAllByAreaLikeOrderByCntDesc(area);
+    }
+
+    public Integer updateCookie(HttpServletResponse response, Cookie countCookie, Long clubId, Integer clubCnt){
+        clubCnt += 1;
+        if(countCookie!=null){
+            String cookieCount = countCookie.getValue();
+            String newValue = cookieCount+"_"+clubId.toString();
+            String[] counts = cookieCount.split("_");
+
+            if(counts.length!=1)
+            {
+                for (String count : counts) {
+                    if (Objects.equals(count, clubId.toString())) {
+                        newValue = cookieCount;
+                        clubCnt -= 1;
+                    }
+                }
+            }else {
+                if(Objects.equals(counts[0], clubId.toString()))
+                {
+                    newValue = cookieCount;
+                    clubCnt -=1;
+                }
+            }
+            countCookie.setValue(newValue);
+            countCookie.setMaxAge(60*60*24);
+            countCookie.setPath("/");
+            response.addCookie(countCookie);
+            return clubCnt;
+        }
+        else {
+            Cookie newCookie = new Cookie("count",clubId.toString());
+            newCookie.setMaxAge(60*60*24);
+            newCookie.setPath("/");
+            response.addCookie(newCookie);
+            return clubCnt;
+        }
+    }
+
+    public void updateCnt(Long clubId, Integer cnt){
+        Optional<Club> club2 = clubRepository.findById(clubId);
+        club2.ifPresent(clubRepository::save);
     }
 
 }
