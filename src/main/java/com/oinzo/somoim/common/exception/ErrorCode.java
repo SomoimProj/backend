07c1c083d96b7759a25e675b8437553b8eca3ea7
yl0937 <60501504+yl0937@users.noreply.github.com>
2023-03-20 15:35:59 +0900

    Chore: ErrorCode 수정
    40002 -> 40003

--- src/main/java/com/oinzo/somoim/common/exception/ErrorCode.java
+++ src/main/java/com/oinzo/somoim/common/exception/ErrorCode.java
@@ -12,8 +12,7 @@ public enum ErrorCode {
 	NO_SEARCH_NAME(HttpStatus.NOT_FOUND, 10001, "검색 키워드를 입력해주십시오."),
 
 	NO_DATA_FOUND(HttpStatus.NOT_FOUND, 40001, "해당하는 데이터를 찾을 수 없습니다."),
-	WRONG_CLUB(HttpStatus.NOT_FOUND, 40002, "해당하는 클럽을 찾을 수 없습니다."),
-
+	WRONG_CLUB(HttpStatus.NOT_FOUND, 40003, "해당하는 클럽을 찾을 수 없습니다."),
 	WRONG_FAVORITE(HttpStatus.NOT_FOUND, 40005, "해당하는 관심사를 찾을 수 없습니다."),
 	/* 500 Internal Server Error */
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 50000, "예상치 못한 오류가 발생했습니다.");
