1e204f9bdf737118d9faded1123322f157e88fe9
Jin YongKyung <jinga5000@naver.com>
2023-03-21 17:09:03 +0900

    chore : 이메일 관련 redis 수정

--- src/main/java/com/oinzo/somoim/domain/user/email/EmailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oinzo.somoim.domain.user.email;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository extends CrudRepository<Email, String> {
-}
--- src/main/java/com/oinzo/somoim/domain/user/email/EmailService.java
+++ src/main/java/com/oinzo/somoim/domain/user/email/EmailService.java
@@ -2,114 +2,124 @@ package com.oinzo.somoim.domain.user.email;
 
 import com.oinzo.somoim.common.exception.BaseException;
 import com.oinzo.somoim.common.exception.ErrorCode;
+import com.oinzo.somoim.domain.user.dto.EmailDto;
 import com.oinzo.somoim.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.time.Duration;
 import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class EmailService {
 
-    @Value("${spring.mail.username}")
-    private String sender;
+	@Value("${spring.mail.username}")
+	private String sender;
 
-    private final JavaMailSender mailSender;
-    private final EmailRepository emailRepository;
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
+	private final RedisTemplate<String, String> redisTemplate;
+	private final JavaMailSender mailSender;
+	private final String EMAIL_PREFIX = "email:";
+	private final long VERIFICATION_CODE_EXPIRATION_TIME = 60 * 3L;
+	private final long VERIFICATION_CODE_CHECK_SUCCESS = 60 * 60 * 24 * 3L;
 
 
-    /**
-     * 인증코드 생성
-     */
-    public String createCode() {
+	/**
+	 * 인증코드 생성
+	 */
+	public String createCode() {
 
-        Random random = new Random();
-        StringBuilder code = new StringBuilder();
+		Random random = new Random();
+		StringBuilder code = new StringBuilder();
 
-        for (int i = 0; i < 8; i++) {
-            int num = random.nextInt(3);
+		for (int i = 0; i < 8; i++) {
+			int num = random.nextInt(3);
 
-            switch (num) {
-                case 0:
-                    code.append((char) (random.nextInt(26) + 97));
-                    break;
-                case 1:
-                    code.append((char) (random.nextInt(26) + 65));
-                    break;
-                case 2:
-                    code.append(random.nextInt(9));
-                    break;
-            }
-        }
+			switch (num) {
+				case 0:
+					code.append((char) (random.nextInt(26) + 97));
+					break;
+				case 1:
+					code.append((char) (random.nextInt(26) + 65));
+					break;
+				case 2:
+					code.append(random.nextInt(9));
+					break;
+			}
+		}
 
-        return code.toString();
-    }
+		return code.toString();
+	}
 
-    /**
-     * 메일 내용 생성
-     */
-    public MimeMessage createMailForm(String email, String code) throws MessagingException {
+	/**
+	 * 메일 내용 생성
+	 */
+	public MimeMessage createMailForm(String email, String code) throws MessagingException {
 
-        String from = sender;
-        String title = "소모임 회원가입을 위한 인증번호";
+		String from = sender;
+		String title = "소모임 회원가입을 위한 인증번호";
 
-        MimeMessage message = mailSender.createMimeMessage();
-        message.addRecipients(MimeMessage.RecipientType.TO, email);
-        message.setSubject(title);
-        message.setFrom(from);
-        message.setText(code, "utf-8", "html");
+		MimeMessage message = mailSender.createMimeMessage();
+		message.addRecipients(MimeMessage.RecipientType.TO, email);
+		message.setSubject(title);
+		message.setFrom(from);
+		message.setText(code, "utf-8", "html");
 
-        return message;
-    }
+		return message;
+	}
 
-    /**
-     * 메일 전송
-     */
-    public String sendMail(String email) throws MessagingException {
-        // 유저테이블에 이메일 존재여부 체크
-        if (userRepository.findByEmail(email).isPresent()) {
-            throw new BaseException(ErrorCode.ALREADY_EXISTS_EMAIL, ErrorCode.ALREADY_EXISTS_EMAIL.getMessage());
-        }
+	/**
+	 * 메일 전송
+	 */
+	public String sendMail(String email) throws MessagingException {
+		// 유저테이블에 이메일 존재여부 체크
+		if (userRepository.findByEmail(email).isPresent()) {
+			throw new BaseException(ErrorCode.ALREADY_EXISTS_EMAIL);
+		}
 
-        String verificationCode = createCode();
-        MimeMessage mailForm = createMailForm(email, verificationCode);
-        mailSender.send(mailForm);
+		String verificationCode = createCode();
+		MimeMessage mailForm = createMailForm(email, verificationCode);
+		mailSender.send(mailForm);
 
-        // mapper
-        Email newEmail = new Email(email, verificationCode, 60 * 3L);
+		redisTemplate.opsForValue().set(
+			EMAIL_PREFIX + email,
+			verificationCode,
+			Duration.ofSeconds(VERIFICATION_CODE_EXPIRATION_TIME)
+		);
 
-        // 레디스에 3분동안 저장
-        try {
-            Email checkEmail = emailRepository.findById(email).get();
-            emailRepository.delete(checkEmail);
-            emailRepository.save(newEmail);
-        } catch (Exception e) {
-            emailRepository.save(newEmail);
-        }
-        return verificationCode;
-    }
+		return verificationCode;
+	}
 
-    /**
-     * 인증코드 검증
-     */
-    public boolean checkVerificationCode(String email, String code) {
+	/**
+	 * 인증코드 검증
+	 */
+	public boolean checkVerificationCode(EmailDto emailDto) {
 
-        Email checkEmail = emailRepository.findById(email).get();
+		if (redisTemplate.opsForValue().get(EMAIL_PREFIX + emailDto.getEmail()) == null) {
+			throw new BaseException(ErrorCode.USER_NOT_FOUND);
+		}
 
-        if (!checkEmail.getCode().equals(code)) {
-            throw new BaseException(ErrorCode.WRONG_VERIFICATION_CODE);
-        }
+		String checkEmail = redisTemplate.opsForValue().get(EMAIL_PREFIX + emailDto.getEmail());
 
-        emailRepository.delete(checkEmail);
-        // 인증 성공 시 데이터를 3일간 저장
-        emailRepository.save(checkEmail);
+		if (!emailDto.getCode().equals(checkEmail)) {
+			throw new BaseException(ErrorCode.WRONG_VERIFICATION_CODE);
+		}
 
-        return true;
-    }
+		redisTemplate.delete(EMAIL_PREFIX + emailDto.getEmail());
+		redisTemplate.opsForValue().set(
+			EMAIL_PREFIX + emailDto.getEmail(),
+			emailDto.getCode(),
+			Duration.ofSeconds(VERIFICATION_CODE_CHECK_SUCCESS)
+		);
+
+		return true;
+	}
 }
