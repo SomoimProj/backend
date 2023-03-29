41236668a65ca6c92f77e38429af1a0c6d1e80f9
soeunkk <luckyber1@naver.com>
2023-03-29 18:30:30 +0900

    test: 클럽 멤버 목록 조회 API (#33)

--- src/test/java/com/oinzo/somoim/domain/clubuser/service/ClubUserServiceTest.java
+++ src/test/java/com/oinzo/somoim/domain/clubuser/service/ClubUserServiceTest.java
@@ -11,12 +11,14 @@ import com.oinzo.somoim.common.exception.BaseException;
 import com.oinzo.somoim.common.exception.ErrorCode;
 import com.oinzo.somoim.common.type.Favorite;
 import com.oinzo.somoim.common.type.ClubUserLevel;
+import com.oinzo.somoim.controller.dto.MemberResponse;
 import com.oinzo.somoim.domain.club.entity.Club;
 import com.oinzo.somoim.domain.club.repository.ClubRepository;
 import com.oinzo.somoim.domain.clubuser.entity.ClubUser;
 import com.oinzo.somoim.domain.clubuser.repository.ClubUserRepository;
 import com.oinzo.somoim.domain.user.entity.User;
 import com.oinzo.somoim.domain.user.repository.UserRepository;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -112,5 +114,63 @@ class ClubUserServiceTest {
 
 		// then
 		assertEquals(ErrorCode.ALREADY_CLUB_MEMBER, exception.getErrorCode());
+	}
+
+	@Test
+	void testGetMembers() {
+		// given
+		Club mockClub = Club.builder()
+			.id(1L)
+			.name("게임 클럽")
+			.description("열정맨 게임 클럽입니다.")
+			.area("청파동")
+			.memberLimit(1)
+			.memberCnt(1)
+			.favorite(Favorite.GAME)
+			.build();
+		User mockUser1 = User.builder()
+			.id(1L)
+			.name("김김김")
+			.profileUrl("url")
+			.introduction("자기소개")
+			.build();
+		User mockUser2 = User.builder()
+			.id(2L)
+			.name("이이이")
+			.build();
+		List<ClubUser> clubUsers = List.of(
+			new ClubUser(1L, ClubUserLevel.MEMBER, mockUser1, mockClub),
+			new ClubUser(2L, ClubUserLevel.MEMBER, mockUser2, mockClub)
+		);
+
+		given(clubRepository.existsById(anyLong()))
+			.willReturn(true);
+		given(clubUserRepository.findByClub_Id(anyLong()))
+			.willReturn(clubUsers);
+
+		// when
+		List<MemberResponse> members = clubUserService.getMembers(1L);
+
+		// then
+		assertEquals(2, members.size());
+		assertEquals(1L, members.get(0).getUserId());
+		assertEquals("김김김", members.get(0).getName());
+		assertEquals("url", members.get(0).getProfileUrl());
+		assertEquals("자기소개", members.get(0).getIntroduction());
+		assertEquals(2L, members.get(1).getUserId());
+		assertEquals("이이이", members.get(1).getName());
+	}
+
+	@Test
+	void testGetMembers_wrongClubId() {
+		given(clubRepository.existsById(anyLong()))
+			.willReturn(false);
+
+		// when
+		BaseException exception = assertThrows(BaseException.class,
+			() -> clubUserService.getMembers(1L));
+
+		// then
+		assertEquals(ErrorCode.WRONG_CLUB, exception.getErrorCode());
 	}
 }
