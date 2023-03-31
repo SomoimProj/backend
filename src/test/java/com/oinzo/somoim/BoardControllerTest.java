2e54adcaab7e89f58199de771231d922af8e6604
yl0937 <yl1033371@gmail.com>
2023-03-31 18:38:21 +0900

    hotfix: BoardTest 카테고리 타입 수정

--- src/test/java/com/oinzo/somoim/BoardControllerTest.java
+++ src/test/java/com/oinzo/somoim/BoardControllerTest.java
@@ -1,5 +1,6 @@
 package com.oinzo.somoim;
 
+import com.oinzo.somoim.common.type.Category;
 import com.oinzo.somoim.controller.dto.BoardCreateRequest;
 import com.oinzo.somoim.controller.dto.BoardResponse;
 import com.oinzo.somoim.domain.board.entity.ClubBoard;
@@ -37,7 +38,7 @@ class BoardControllerTest {
     @DisplayName("게시판 생성 테스트")
     void addBoard() {
         // given
-        BoardCreateRequest newBoard = new BoardCreateRequest("NORMAL","게시판 테스트","URL","자유");
+        BoardCreateRequest newBoard = new BoardCreateRequest("NORMAL","게시판 테스트","URL", "FREE");
         Club club = clubRepository.findById(1L).orElseThrow();
         User user = userRepository.findById(7L).orElseThrow();
         // when
@@ -51,21 +52,21 @@ class BoardControllerTest {
     void readBoard() {
         // given
         // when
-        BoardResponse board = clubBoardService.readBoard(1L);
+        BoardResponse board = clubBoardService.readBoard(160L);
         // then
-        assertEquals("제목", board.getTitle());
+        assertEquals("NORMAL", board.getTitle());
     }
 
     @Test
     @DisplayName("게시판 수정 테스트")
     void updateBoard() {
         // given
-        BoardCreateRequest newBoard = new BoardCreateRequest("NORMAL","게시판 테스트","URL","자유");
+        BoardCreateRequest newBoard = new BoardCreateRequest("NORMAL","게시판 테스트","URL","FREE");
         Club club = clubRepository.findById(1L).orElseThrow();
         User user = userRepository.findById(7L).orElseThrow();
         ClubBoard board = clubBoardRepository.save(ClubBoard.from(newBoard,club,user));
         // when
-        BoardCreateRequest updateBoard = new BoardCreateRequest("NORMAL","수정된 테스트","URL","자유");
+        BoardCreateRequest updateBoard = new BoardCreateRequest("NORMAL","수정된 테스트","URL","FREE");
         BoardResponse updateBoard1 =clubBoardService.updateBoard(board.getId(),updateBoard,7L);
         assertEquals("NORMAL", updateBoard1.getTitle());
     }
