96c39240c7575f6f596d86a41a437c9d0ea25bb7
yl0937 <yl1033371@gmail.com>
2023-04-02 18:19:12 +0900

    chore: 앨범 좋아요 반영 및 페이징 재적용

--- src/main/java/com/oinzo/somoim/controller/ClubAlbumController.java
+++ src/main/java/com/oinzo/somoim/controller/ClubAlbumController.java
@@ -6,7 +6,8 @@ import com.oinzo.somoim.controller.dto.AlbumResponse;
 import com.oinzo.somoim.common.response.SuccessResponse;
 import com.oinzo.somoim.domain.album.service.ClubAlbumService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,28 +22,36 @@ public class ClubAlbumController {
 
     @PostMapping("{clubId}/albums")
     public SuccessResponse<AlbumResponse> addAlbum(@AuthenticationPrincipal Long userId,
-                                  @RequestBody @Valid AlbumCreateRequest request, @PathVariable Long clubId){
-        AlbumResponse albumResponse = clubAlbumService.addAlbum(request,userId,clubId);
+                                                   @RequestBody @Valid AlbumCreateRequest request, @PathVariable Long clubId) {
+        AlbumResponse albumResponse = clubAlbumService.addAlbum(request, userId, clubId);
         return ResponseUtil.success(albumResponse);
     }
 
     @GetMapping("{clubId}/albums")
-    public SuccessResponse<List<AlbumResponse>> readAllAlbum(@PathVariable Long clubId, Pageable pageable){
-        List<AlbumResponse> albumResponses = clubAlbumService.readAllAlbum(clubId,pageable);
+    public SuccessResponse<List<AlbumResponse>> readAllAlbum(@PathVariable Long clubId) {
+        List<AlbumResponse> albumResponses = clubAlbumService.readAllAlbumList(clubId);
+        return ResponseUtil.success(albumResponses);
+    }
+
+    @GetMapping("{clubId}/albums/_page={pageNum}")
+    public SuccessResponse<Page<AlbumResponse>> readAllAlbumByPaging(@PathVariable Long clubId,
+                                                             @PathVariable int pageNum) {
+        PageRequest pageable = PageRequest.of(pageNum,10);
+        Page<AlbumResponse> albumResponses = clubAlbumService.readAllAlbum(clubId, pageable);
         return ResponseUtil.success(albumResponses);
     }
 
     @GetMapping("/albums/{albumId}")
     public SuccessResponse<AlbumResponse> readAlbum(@PathVariable Long albumId,
-                                   @AuthenticationPrincipal Long userId){
-        AlbumResponse albumResponse = clubAlbumService.readOneAlbum(albumId,userId);
+                                                    @AuthenticationPrincipal Long userId) {
+        AlbumResponse albumResponse = clubAlbumService.readOneAlbum(albumId, userId);
         return ResponseUtil.success(albumResponse);
     }
 
     @DeleteMapping("/albums/{albumId}")
     public SuccessResponse<?> deleteAlbum(@PathVariable Long albumId,
-                            @AuthenticationPrincipal Long userId){
-        clubAlbumService.deleteAlbum(albumId,userId);
+                                          @AuthenticationPrincipal Long userId) {
+        clubAlbumService.deleteAlbum(albumId, userId);
         return ResponseUtil.success();
     }
 }
--- src/main/java/com/oinzo/somoim/domain/album/service/ClubAlbumService.java
+++ src/main/java/com/oinzo/somoim/domain/album/service/ClubAlbumService.java
@@ -6,18 +6,21 @@ import com.oinzo.somoim.controller.dto.AlbumCreateRequest;
 import com.oinzo.somoim.controller.dto.AlbumResponse;
 import com.oinzo.somoim.domain.album.entity.ClubAlbum;
 import com.oinzo.somoim.domain.album.repository.ClubAlbumRepository;
-import com.oinzo.somoim.domain.club.entity.Club;
+import com.oinzo.somoim.domain.albumlike.repository.AlbumLikeRepository;
 import com.oinzo.somoim.domain.club.repository.ClubRepository;
 import com.oinzo.somoim.domain.clubuser.repository.ClubUserRepository;
 import com.oinzo.somoim.domain.user.entity.User;
 import com.oinzo.somoim.domain.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -26,42 +29,58 @@ public class ClubAlbumService {
     private final UserRepository userRepository;
     private final ClubRepository clubRepository;
     private final ClubUserRepository clubUserRepository;
+    private final AlbumLikeRepository likeRepository;
 
-    public AlbumResponse addAlbum(AlbumCreateRequest request, Long userId, Long clubId){
+    public AlbumResponse addAlbum(AlbumCreateRequest request, Long userId, Long clubId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(()-> new BaseException(ErrorCode.USER_NOT_FOUND));
-        if(!clubRepository.existsById(clubId))
-                throw new BaseException(ErrorCode.WRONG_CLUB);
-        if(!clubUserRepository.existsByUser_IdAndClub_Id(userId,clubId)) throw new BaseException(ErrorCode.NOT_CLUB_MEMBER);
-        return AlbumResponse.from(clubAlbumRepository.save(ClubAlbum.from(request,user,clubId)));
+                .orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+        if (!clubRepository.existsById(clubId))
+            throw new BaseException(ErrorCode.WRONG_CLUB);
+        if (!clubUserRepository.existsByUser_IdAndClub_Id(userId, clubId))
+            throw new BaseException(ErrorCode.NOT_CLUB_MEMBER);
+        return AlbumResponse.from(clubAlbumRepository.save(ClubAlbum.from(request, user, clubId)), 0);
     }
 
-    public List<AlbumResponse> readAllAlbum(Long clubId,Pageable pageable){
+    public List<AlbumResponse> readAllAlbumList(Long clubId) {
         if (!clubRepository.existsById(clubId)) {
             throw new BaseException(ErrorCode.WRONG_CLUB);
         }
-        List<ClubAlbum> albumList = clubAlbumRepository.findAllByClubId(clubId,pageable).getContent();
-        return AlbumResponse.listToAlbumResponse(albumList);
+        List<ClubAlbum> albums = clubAlbumRepository.findAllByClubId(clubId);
+        return albums.stream()
+                .map(album -> AlbumResponse.from(album, likeRepository.countAllByAlbum_Id(album.getId())))
+                .collect(Collectors.toList());
     }
 
-    public AlbumResponse readOneAlbum(Long albumId,Long userId){
+    public Page<AlbumResponse> readAllAlbum(Long clubId, Pageable pageable) {
+        if (!clubRepository.existsById(clubId)) {
+            throw new BaseException(ErrorCode.WRONG_CLUB);
+        }
+        List<ClubAlbum> albums = clubAlbumRepository.findAllByClubId(clubId, pageable).getContent();
+        return new PageImpl<>(albums.stream()
+                .map(album -> AlbumResponse.from(album, likeRepository.countAllByAlbum_Id(album.getId())))
+                .collect(Collectors.toList()));
+    }
+
+    public AlbumResponse readOneAlbum(Long albumId, Long userId) {
         if (!userRepository.existsById(userId)) {
             throw new BaseException(ErrorCode.USER_NOT_FOUND);
         }
         ClubAlbum album = clubAlbumRepository.findById(albumId)
-                .orElseThrow(()-> new BaseException(ErrorCode.WRONG_ALBUM));
-        if(!clubUserRepository.existsByUser_IdAndClub_Id(userId,album.getClubId())) throw new BaseException(ErrorCode.NOT_CLUB_MEMBER);
-        return AlbumResponse.from(album);
+                .orElseThrow(() -> new BaseException(ErrorCode.WRONG_ALBUM));
+        if (!clubUserRepository.existsByUser_IdAndClub_Id(userId, album.getClubId()))
+            throw new BaseException(ErrorCode.NOT_CLUB_MEMBER);
+        return AlbumResponse.from(album, likeRepository.countAllByAlbum_Id(albumId));
     }
 
     @Transactional
-    public void deleteAlbum(Long albumId, Long userId){
+    public void deleteAlbum(Long albumId, Long userId) {
         if (!userRepository.existsById(userId)) {
             throw new BaseException(ErrorCode.USER_NOT_FOUND);
         }
         ClubAlbum album = clubAlbumRepository.findById(albumId)
-                .orElseThrow(()-> new BaseException(ErrorCode.WRONG_ALBUM));
-        if(!Objects.equals(album.getUser().getId(),userId)) throw new BaseException(ErrorCode.FORBIDDEN_REQUEST,"작성자가 아닙니다.");
+                .orElseThrow(() -> new BaseException(ErrorCode.WRONG_ALBUM));
+        if (!Objects.equals(album.getUser().getId(), userId))
+            throw new BaseException(ErrorCode.FORBIDDEN_REQUEST, "작성자가 아닙니다.");
         clubAlbumRepository.delete(album);
     }
 }
